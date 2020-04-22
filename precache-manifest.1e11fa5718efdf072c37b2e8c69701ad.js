084362f6b3c03faaea67cdea23e8e051bc3104d2
Teppo A <Tepia94@gmail.com>
2020-04-22 09:17:11 +0300

    Updates

---
+++ precache-manifest.1e11fa5718efdf072c37b2e8c69701ad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ae86b0f5ef8a237d93efaf101dc4237",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b84949110dca4b45624e",
+    "url": "/portfolio/static/css/2.397eae54.chunk.css"
+  },
+  {
+    "revision": "3510c30b7d3257ed8c44",
+    "url": "/portfolio/static/css/main.271e17b7.chunk.css"
+  },
+  {
+    "revision": "b84949110dca4b45624e",
+    "url": "/portfolio/static/js/2.4aa12ca9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.4aa12ca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3510c30b7d3257ed8c44",
+    "url": "/portfolio/static/js/main.c13e62f4.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "217fe5c4cd5b2feb440bd39a1fa37e17",
+    "url": "/portfolio/static/media/ApartmentImg.217fe5c4.png"
+  },
+  {
+    "revision": "de32becee0454ea54cdcaa3e7a06054f",
+    "url": "/portfolio/static/media/CPP-PB-JUN2019.de32bece.png"
+  },
+  {
+    "revision": "7babf18646ab05eaeee2970330a69418",
+    "url": "/portfolio/static/media/CS-ADV-JAN2020.7babf186.png"
+  },
+  {
+    "revision": "e21974f10009db75a91c1c73dd2b32ea",
+    "url": "/portfolio/static/media/CS-FUND-SEP2019.e21974f1.png"
+  },
+  {
+    "revision": "f555f424ec6a8c59a5e26dee1d893ce0",
+    "url": "/portfolio/static/media/CS-OOP-FEB2020.f555f424.png"
+  },
+  {
+    "revision": "e7b8fcd4ceb430ffc60ed55e5ab98fa5",
+    "url": "/portfolio/static/media/CSharpLogo.e7b8fcd4.png"
+  },
+  {
+    "revision": "77a85821b0f884a5f7e4bc3c677cea95",
+    "url": "/portfolio/static/media/SelfImage2.77a85821.jpg"
+  },
+  {
+    "revision": "153762cfb3007108140ab359487df515",
+    "url": "/portfolio/static/media/UnityLogo.153762cf.png"
+  }
+]);
