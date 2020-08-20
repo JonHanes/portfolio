466b0d1c38f46547537fa37959b5ee5d6ce4c08d
Teppo A <Tepia94@gmail.com>
2020-08-20 21:24:55 +0300

    Finally added the filters for going through the cheatsheet, one certificate added, image gallery updated but still needs some sorting out before further implementation.

--- src/Components/ImageGallery.js
+++ src/Components/ImageGallery.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 //#region Bootstrap Dependencies
 import Container from 'react-bootstrap/Container';
 import Image from 'react-bootstrap/Image';
+import {Navbar, Nav, Button} from 'react-bootstrap';
 //#endregion
 
 //#region Internal Dependencies
@@ -19,7 +20,7 @@ import Unity3DEssentials2020 from "../Assets/Images/Certificates/Unity3D_Essenti
 import ZENVA_MICROVR from "../Assets/Images/Certificates/Zenva_BuildMicro-VRGame.PNG"
 import ZENVA_AR_INTRO from "../Assets/Images/Certificates/Zenva_IntroToAR.PNG"
 import ZENVA_NodeJS_Express_BGN from "../Assets/Images/Certificates/Zenva_Node.js&Express_BGN.PNG"
-
+import ZENVA_React_Webforms from "../Assets/Images/Certificates/Zenva_React_Webforms.PNG"
 //Udemy
 import UDEMY_2D_Course from "../Assets/Images/Certificates/Udemy_Unity_2D_Course.jpg"
 
@@ -27,90 +28,171 @@ import UDEMY_2D_Course from "../Assets/Images/Certificates/Udemy_Unity_2D_Course
 
 class ImageGallery extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            softuni_visible: true,
+            zenva_visible: true,
+            udemy_visible: true,
+        };
+    }
+
+    handleToggle(filter) {
+
+        switch(filter) {
+            case "SoftUni":
+                this.state.softuni_visible ? this.setState({softuni_visible: false}) : this.setState({softuni_visible: true});
+                break;
+            case "Zenva":
+                this.state.zenva_visible ? this.setState({zenva_visible: false}) : this.setState({zenva_visible: true});
+                break;
+            case "Udemy":
+                this.state.udemy_visible ? this.setState({udemy_visible: false}) : this.setState({udemy_visible: true});
+                break;
+            default:
+                break;
+        }
+
+        
+    } 
+
     render () {
         return (
             <div>
-    
                 <h5 className="centeredText">
                     Current Average Score: <b>5.96/6.00</b><br/>
                 </h5>
-    
-                <Container className="contentCentering" fluid>
+
+                {/* 
+                    Implementing this once the issues with spacing are cleared up
+                    <Navbar bg="dark" variant="dark">
+                        <Navbar.Brand href="#home">Filter</Navbar.Brand>
+                            <Nav className="mr-auto">
+                                <Button variant="primary" size="lg" className={this.state.softuni_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("SoftUni")}>SoftUni</Button>
+                                <Button variant="primary" size="lg" className={this.state.zenva_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("Zenva")}>Zenva</Button>
+                                <Button variant="primary" size="lg" className={this.state.udemy_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("Udemy")}>Udemy</Button>
+                        </Nav>   
+                    </Navbar>
+                */}
+                
+                
+                <Container className="contentCentering" fluid >
+                    {this.state.softuni_visible ? 
                         <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={CPPPBJune2019} thumbnail/>
-                            <h6>C++ Programming Basics Certificate (6.00/6.00) <br/>
-                            <i>July 2019</i></h6>
+                        <Image className="gallery-smallImg" src={CPPPBJune2019} thumbnail/>
+                        <h6>C++ Programming Basics Certificate (6.00/6.00) <br/>
+                        <i>July 2019</i></h6>
                         </div>
-    
+                        : ''
+                     }
+                        
+                    { this.state.softuni_visible ? 
                         <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={CSFUNDSEP2019} thumbnail />
-                            <h6>C# Fundamentals Certificate (6.00/6.00) <br/>
-                            <i>December 2019</i></h6>
+                        <Image className="gallery-smallImg" src={CSFUNDSEP2019} thumbnail />
+                        <h6>C# Fundamentals Certificate (6.00/6.00) <br/>
+                        <i>December 2019</i></h6>
                         </div>
-    
+                        : ''
+                    }
+                    
+                    { this.state.softuni_visible ? 
                         <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={CSADVJAN2020} thumbnail />
-                            <h6>C# Advanced Certificate (6.00/6.00) <br/>
-                            <i>March 2020</i></h6>
+                        <Image className="gallery-smallImg" src={CSADVJAN2020} thumbnail />
+                        <h6>C# Advanced Certificate (6.00/6.00) <br/>
+                        <i>March 2020</i></h6>
                         </div>
-    
+                        : ''
+                    }
+
+                    { this.state.softuni_visible ? 
                         <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={CSOOPFEB2020} thumbnail />
-                            <h6>C# OOP Certificate (6.00/6.00) <br/>
-                            <i>April 2020</i></h6>
+                        <Image className="gallery-smallImg" src={CSOOPFEB2020} thumbnail />
+                        <h6>C# OOP Certificate (6.00/6.00) <br/>
+                        <i>April 2020</i></h6>
                         </div>
+                        : ''
+                    }
+                    
+                    {/*Need to come up with another solution for spacing in the near future, maybe an array of all the components*/}
+                        <br/>
+                        <br/>
+
+                    { this.state.softuni_visible ? 
+                        <div className="gallery-imageWrapper">
+                        <Image className="gallery-smallImg" src={MSSQL_Basics} thumbnail />
+                        <h6>Databases Basics - Microsoft SQL Server (6.00/6.00)<br/>
+                        <i>June 2020</i></h6>
+                        </div>
+                        : ''
+                    }
+
+                    
+                    { this.state.softuni_visible ? 
+                        <div className="gallery-imageWrapper">
+                        <Image className="gallery-smallImg" src={CS_EF} thumbnail />
+                        <h6>Entity Framework Core (5.82/6.00)<br/>
+                        <i>July 2020</i></h6>
+                        </div>
+                        : ''
+                    }
+                        
+                    { this.state.softuni_visible ? 
+                        <div className="gallery-imageWrapper">
+                        <Image className="gallery-smallImg" src={Unity3DEssentials2020} thumbnail />
+                        <h6>Unity 3D Essentials (6.00/6.00 - First place)<br/>
+                        <i>July 2020</i></h6>
+                        </div>
+                        : ''
+                    }
+                        
+                    { this.state.zenva_visible ? 
+                        <div className="gallery-imageWrapper">
+                        <Image className="gallery-smallImg" src={ZENVA_MICROVR} thumbnail />
+                        <h6>Zenva - Build a Micro-VR Game<br/>
+                        <i>April 2020</i></h6>
+                        </div>
+                        : ''
+                    }
 
                         <br/>
                         <br/>
 
+                    { this.state.zenva_visible ? 
                         <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={MSSQL_Basics} thumbnail />
-                            <h6>Databases Basics - Microsoft SQL Server (6.00/6.00)<br/>
-                            <i>June 2020</i></h6>
+                        <Image className="gallery-smallImg" src={ZENVA_AR_INTRO} thumbnail />
+                        <h6>Zenva - Intro to Augmented Reality<br/>
+                        <i>April 2020</i></h6>
                         </div>
+                        : ''
+                    }
 
+                    { this.state.zenva_visible ? 
                         <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={CS_EF} thumbnail />
-                            <h6>Entity Framework Core (5.82/6.00)<br/>
-                            <i>July 2020</i></h6>
+                        <Image className="gallery-smallImg" src={ZENVA_NodeJS_Express_BGN} thumbnail />
+                        <h6>Zenva - Node.js and Express for Beginners<br/>
+                        <i>July 2020</i></h6>
                         </div>
+                        : ''
+                    }
 
+                    { this.state.zenva_visible ? 
+                        <div className="gallery-imageWrapper">
+                        <Image className="gallery-smallImg" src={ZENVA_React_Webforms} thumbnail />
+                        <h6>Zenva - Craft Web Forms with React<br/>
+                        <i>August 2020</i></h6>
+                        </div>
+                        : ''
+                    }
+
+                    { this.state.udemy_visible ? 
+                        <div className="gallery-imageWrapper">
+                        <Image className="gallery-smallImg" src={UDEMY_2D_Course} thumbnail />
+                        <h6>Complete C# Unity Developer 2D<br/>
+                        <i>May 2020</i></h6>
+                        </div>
+                        : ''
+                    }                    
                         
-                        
-                        <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={Unity3DEssentials2020} thumbnail />
-                            <h6>Unity 3D Essentials (6.00/6.00 - First place)<br/>
-                            <i>July 2020</i></h6>
-                        </div>
-
-                        
-                        <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={ZENVA_MICROVR} thumbnail />
-                            <h6>Zenva - Build a Micro-VR Game<br/>
-                            <i>April 2020</i></h6>
-                        </div>
-
-                        <br/>
-                        <br/>
-
-                        <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={ZENVA_AR_INTRO} thumbnail />
-                            <h6>Zenva - Intro to Augmented Reality<br/>
-                            <i>April 2020</i></h6>
-                        </div>
-
-                        <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={ZENVA_NodeJS_Express_BGN} thumbnail />
-                            <h6>Zenva - Node.js and Express for Beginners<br/>
-                            <i>July 2020</i></h6>
-                        </div>
-                        
-                        <div className="gallery-imageWrapper">
-                            <Image className="gallery-smallImg" src={UDEMY_2D_Course} thumbnail />
-                            <h6>Complete C# Unity Developer 2D<br/>
-                            <i>May 2020</i></h6>
-                        </div>
-
                         <br/>
                         <p className="boldTextWithTopMargin">...and more to come as I'm still studying...</p>
                 </Container>
--- src/Pages/Cheatsheet.js
+++ src/Pages/Cheatsheet.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Navbar, Nav, Button} from 'react-bootstrap';
 
 //#region Internal Dependencies
 import Hero from "../Components/Hero";
@@ -14,30 +15,89 @@ import GitHubCheatsheet from '../Components/Fragments/CS_GitHub';
 import ReactCheetsheet from '../Components/Fragments/CS_React';
 //#endregion
 
-function Cheatsheet(props) {
-    return (
-        <div>
-            <Hero title={props.title}/>
-            
-            <Content>
-                <h4 className="explanationText">Very much a work in progress.<br/>Functions to add:<br/>
-                <ul>
-                    <li>Collapsible elements [Useful when this grows in scale]</li>
-                    <li>Add styling to the divs I am currently using.</li>
-                    <li>A navbar that is fixed to either side.</li>
-                </ul>
-                </h4>
-                
-                <CSharpCheatsheet/>
-                <JSCheatsheet/>
-                <ReactCheetsheet/>
-                <PHPCheatsheet/>
-                <MiscCheatsheet/>
-                <GitHubCheatsheet/>
+class Cheatsheet extends React.Component {
 
-            </Content>
-        </div>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            cs_visible: true,
+            js_visible: true,
+            react_visible: true,
+            php_visible: true,
+            misc_visible: true,
+            gh_visible: true,
+        };
+    }
+
+    handleToggle(filter) {
+
+        switch(filter) {
+            case "C#":
+                this.state.cs_visible ? this.setState({cs_visible: false}) : this.setState({cs_visible: true});
+                break;
+            case "JS":
+                this.state.js_visible ? this.setState({js_visible: false}) : this.setState({js_visible: true});
+                break;
+            case "React":
+                this.state.react_visible ? this.setState({react_visible: false}) : this.setState({react_visible: true});
+                break;
+            case "PHP":
+                this.state.php_visible ? this.setState({php_visible: false}) : this.setState({php_visible: true});
+                break;
+            case "Misc":
+                this.state.misc_visible ? this.setState({misc_visible: false}) : this.setState({misc_visible: true});
+                break;
+            case "GitHub":
+                this.state.gh_visible ? this.setState({gh_visible: false}) : this.setState({gh_visible: true});
+                break;
+            default:
+                break;
+        }
+
+        
+    }    
+
+
+    
+    render() {
+        return (
+            <div>
+                <Navbar bg="dark" variant="dark">
+                        <Navbar.Brand href="#home">Filter</Navbar.Brand>
+                            <Nav className="mr-auto">
+                                <Button variant="primary" size="lg" className={this.state.cs_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("C#")}>C#</Button>
+                                <Button variant="primary" size="lg" className={this.state.js_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("JS")}>JS</Button>
+                                <Button variant="primary" size="lg" className={this.state.react_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("React")}>React</Button>
+                                <Button variant="primary" size="lg" className={this.state.php_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("PHP")}>PHP</Button>
+                                <Button variant="primary" size="lg" className={this.state.misc_visible ? "navBarItem" : "navBarItemDeselected"} onClick={() => this.handleToggle("Misc")}>Misc</Button>
+                                <Button variant="primary" size="lg" className={this.state.gh_visible ? "navBarItem" : "navBarItemDeselected"}  onClick={() => this.handleToggle("GitHub")}>GitHub</Button>
+                        </Nav>   
+                </Navbar>
+                <Hero title={this.props.title}/>
+            
+                <Content>
+                    <h4 className="explanationText">Very much a work in progress.<br/>Functions to add:<br/>
+                    <ul>
+                        <li>Collapsible elements [Useful when this grows in scale]</li>
+                        <li>Add styling to the divs I am currently using.</li>
+                    </ul>
+                    </h4>
+                    
+                    
+                    
+                    {this.state.cs_visible ? <CSharpCheatsheet/> : ''}
+                    {this.state.js_visible ? <JSCheatsheet/> : ''}
+                    {this.state.react_visible ? <ReactCheetsheet/> : ''}
+                    {this.state.php_visible ? <PHPCheatsheet/> : ''}
+                    {this.state.misc_visible ? <MiscCheatsheet/> : ''}
+                    {this.state.gh_visible ? <GitHubCheatsheet/> : ''}
+    
+                </Content>
+            </div>
+        
+        )
+    }   
+    
 }
 
 export default Cheatsheet;
